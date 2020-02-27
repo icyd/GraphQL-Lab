b28cec99b5ef6fdf575a233d37e6b6ff63f995d7
Alberto Vázquez <beto.v25@gmail.com>
2020-02-27 09:33:22 -0500

    Partial commit

--- server/app.ts
+++ server/app.ts
@@ -1,20 +1,18 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
+import express from 'express';
 // import graphqlHTTP from 'express-graphql';
 // import schema from './schema/schema';
 // import mongoose from 'mongoose';
 // import cors from 'cors';
-var app = express_1.default();
+
+
+const app: express.Application = express();
 // app.use(cors());
 // mongoose.set('useUnifiedTopology', true);
 // mongoose.connect('mongodb://test:1234@localhost:27017/mydb?authSource=admin', { useNewUrlParser: true });
 // mongoose.connection.once('open', () => {
 //     console.log('conneted to database');
 // });
+
 // app.use('/graphql', graphqlHTTP({
 //   schema: schema,
 //   graphiql: true
@@ -22,9 +20,10 @@ var app = express_1.default();
 //
 console.log(typeof app);
 console.log(app.type);
-app.use('/', function (req, res) {
-    res.send('Hello world');
+app.use('/', (req, res) => {
+  res.send('Hello world');
 });
-app.listen(4000, function () {
-    console.log('Listening on port 4000.');
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000.');
 });
